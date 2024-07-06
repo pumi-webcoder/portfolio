6d86b3d9d772036491579f3ced18812367f10296
pumi-webcoder <pumi.webcoder@gmail.com>
2024-07-06 16:50:47 +0900

    不要ファイル整理

--- taihi/breadcrumb.php
+++
@@ -1,6 +0,0 @@
-<?php if (function_exists('bcn_display')) : ?>
-<!-- breadcrumb -->
-<div class="breadcrumb">
-	<?php bcn_display(); // BreadcrumbNavXTのパンくずを表示するための記述 ?>
-</div><!-- /breadcrumb -->
-<?php endif; ?>
